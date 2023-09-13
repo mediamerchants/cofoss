fbd50806ac09cc8b1c25a3f37ad5487f4d020467
Julius <julius@mediamerchants.com.au>
2023-09-14 07:04:00 +0800

    COF-12 | fix alignment issues on about us page, put conditional statement for the slider title on homepage slider

--- wp-content/themes/cofoss/resources/views/components/banner-slider-component.blade.php
+++ wp-content/themes/cofoss/resources/views/components/banner-slider-component.blade.php
@@ -11,8 +11,12 @@
             <div class="swiper-slide">
                 {!! $image['url'] !!}
                 <div class="banner-text">
+                    @if( $image['title'] ) 
                     <h1 class="has-text-align-left bold-heading-text">{!! $image['title'] !!}</h1>
+                    @endif
+                    @if( $image['subtext'] ) 
                     <p>{!! $image['subtext'] !!}</p>
+                    @endif
                 </div>
             </div>
        @endforeach
